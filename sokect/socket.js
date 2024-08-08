5f4b62b45e0675639e37c7f5f8626d01cc4ebb6e
Rishabh Rathore <rishabhrathore@Rishabhs-MacBook-Pro.local>
2024-08-08 08:52:14 +0530

    final

--- sokect/socket.js
+++ sokect/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server , {
     cors :{
-        origin : ["https://chit-chat-fe-mu.vercel.app"],
+        origin : ["http://localhost:3000"],
         methods : ["GET" , "POST"]
     }
 });
